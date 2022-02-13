d894d29338155eef20f62baa7a17abbac9ebc2c8
Kotyara <babanina.ev@gmail.com>
2022-02-13 22:02:51 +0200

    add tabels in Database script

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -5,3 +5,30 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_front'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 USE database;
+
+create table roles
+(
+    id int auto_increment primary key,
+    name ENUM('CUSTOM', 'ADMIN')
+);
+create table users
+(
+    id       int auto_increment
+        primary key,
+    username varchar(30)  not null,
+    password varchar(120) not null,
+    email    varchar(40)  not null,
+    role_id int not null,
+    constraint users_email_uindex
+        unique (email),
+    constraint role_id
+        foreign key (role_id) references roles (id)
+);
+create table products
+(
+    id       int auto_increment
+        primary key,
+    name varchar(80)  not null,
+    price varchar(40)  not null,
+    description varchar(500)
+);
