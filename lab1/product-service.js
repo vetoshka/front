9b573eb4e67353818d9b2f751a4de34410627c49
Anna <Anna_Kolomiiets>
2022-02-16 10:22:06 +0200

    add home-page

---
+++ lab1/product-service.js
@@ -0,0 +1,55 @@
+let products = JSON.stringify([
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product1.jpg"
+    },
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product2.jpg"
+    },
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product3.jpg"
+    }
+    ,
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product4.jpg"
+    }
+    ,
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product5.jpg"
+    }
+    ,
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product6.jpg"
+    }
+    ,
+    {
+        "name": "I'm a product",
+        "price": 15.00,
+        "image": "./images/product7.jpg"
+    }
+]);
+
+
+window.onload = function() {
+const container = document.querySelector(".product_list");
+JSON.parse(products).forEach(e => {
+    container.insertAdjacentHTML('beforeend', 
+    `<div class="product">
+        <img class= "product_image" src="${e.image}">
+        <span class="product_name">${e.name}</span>
+        <div class="line"></div>
+        <span class="product_price">$${e.price}</span>
+    </div>` 
+    );
+})}
