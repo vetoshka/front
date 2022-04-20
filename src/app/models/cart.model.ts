261ed29163b947faf6daacc9c85655aee4555d94
vetoshka <43034693+vetoshka@users.noreply.github.com>
2022-04-20 22:20:33 +0000

    Deploying to gh-pages from  @ 6a7b3f65a04bc17c7d27fa56a2bd5eb7dda87955 🚀

--- src/app/models/cart.model.ts
+++
@@ -1,7 +0,0 @@
-export interface CartModel {
-    id: number;
-    name: string;
-    price: number;
-    image: string;
-    quantity:number;
-}
--- src/app/services/product.config.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-export const ProductsAPI = new InjectionToken<string>('ProductsAPI', {
-    providedIn: 'any',
-    factory: () => 'http://localhost:3000/products'
-});
